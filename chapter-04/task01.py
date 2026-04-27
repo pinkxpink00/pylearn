fcf2b2e7219a3f01f8367259d25b72acea72bbc1
pinkXpink00 <kyperu07@gmail.com>
2026-04-28 00:29:58 +0300

    task1

---
+++ chapter-04/task01.py
@@ -0,0 +1,8 @@
+#Создай список из 5 любых чисел. Выведи сумму, минимум, максимум и отсортированный список.
+
+digits = [20,26,23,11,10]
+
+print(sum(digits))
+print(min(digits))
+print(max(digits))
+print(sorted(digits))
